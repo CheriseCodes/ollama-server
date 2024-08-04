929d6d99e11cc5be3244ecf24a49f70e50542063
CheriseCodes <cherise.bry@gmail.com>
2024-08-04 11:38:24 -0400

    add instance config to inputs

--- inputs.tf
+++ inputs.tf
@@ -23,3 +23,21 @@ variable "key_pair_name" {
   description = "Name of EC2 keypair to SSH into the server"
   nullable    = true
 }
+
+variable "volume_size" {
+  type        = number
+  description = "The amount of space on the Ollama server"
+  default     = 50
+  nullable    = false
+}
+
+variable "instance_type" {
+  type        = string
+  description = "The type of EC2 instance used by the Ollama server"
+  default     = "g4dn.xlarge"
+  validation {
+    condition     = contains(["g4dn.xlarge", "g4dn.2xlarge", "g4dn.4xlarge", "g4dn.8xlarge", "g4dn.16xlarge", "g4dn.12xlarge", "g4dn.metal"], var.instance_type)
+    error_message = "An Nvidia enabled instance type must be used. Valid values for var: instance_type are (g4dn.xlarge, g4dn.2xlarge, g4dn.4xlarge, g4dn.8xlarge, g4dn.16xlarge, g4dn.12xlarge, g4dn.metal)."
+  }
+  nullable = false
+}

e48c800f9cf00ec84e71f9e3ee4c958110248aa4
CheriseCodes <cherise.bry@gmail.com>
2024-09-21 19:47:15 -0400

    auto reload after install script modification

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -2,7 +2,8 @@
 set -x
 export HOME=/home/ubuntu
 curl -fsSL https://ollama.com/install.sh | sed 's/Environment="\PATH=$PATH\"/Environment=\"OLLAMA_HOST=0.0.0.0\"/g' | sh
-sudo systemctl start ollama.service
+systemctl daemon-reload
+systemctl restart ollama.service
 ollama pull gemma2
 ollama pull mistral
 ollama pull llama3.1
